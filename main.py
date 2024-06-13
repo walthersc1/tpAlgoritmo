a72939f38a80ff88480e2a8192985a4924180d6d
walthersc11 <gamersc11@hotmail.com>
2024-06-12 23:34:55 -0500

    commit 10

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
 from fastapi.middleware.cors import CORSMiddleware
@@ -13,32 +13,28 @@ app = FastAPI()
 
 # Configurar CORS
 origins = [
-    "http://localhost:3000/**",  # Origen de desarrollo local
-    "https://proyecto-eetmxget9-walthers-projects-76c0417b.vercel.app/",  
-    "https://proyecto-eetmxget9-walthers-projects-76c0417b.vercel.app/Chatbot/",
-    "https://proyecto-tp-chi.vercel.app/**",
-    "https://tpalgoritmo-production.up.railway.app/**",# Reemplaza con tu dominio de Vercel 
+    "http://localhost:3000",  # Origen de desarrollo local
+    "https://proyecto-6j9n8jmjo-walthersc11s-projects.vercel.app",  # Reemplaza con tu dominio de Vercel
     # Añade otros orígenes permitidos aquí
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
 # Definir el modelo de datos para la entrada
 class TextInput(BaseModel):
     text: str
 
-@app.get("/mapri/")
-def mapri():
-    return print("Hola mundo")
+@app.options("/{rest_of_path:path}")
+async def preflight_check():
+    return {"message": "This is a preflight response"}
 
-@app.get("/predict/")
+
+@app.post("/predict/")
 def predict(input_data: TextInput):
     # Transformar los datos de entrada usando el vectorizador
     transformed_data = vectorizer.transform([input_data.text])
@@ -47,11 +43,6 @@ def predict(input_data: TextInput):
     # Retornar la predicción como respuesta
     return {"prediction": prediction.tolist()}
 
-"""
-
-
-
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
-"""
